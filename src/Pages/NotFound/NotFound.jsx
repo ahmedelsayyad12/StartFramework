2de4180d92570f5b055708d2e8c95b7da7ee939a
ahmedelsayyad12 <47593464+ahmedelsayyad12@users.noreply.github.com>
2024-11-27 18:18:30 +0200

    Add files via upload

---
+++ src/Pages/NotFound/NotFound.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+import Not from '../../assets/images/Desktop - 4.png'
+
+
+export default function NotFound() {
+  return (
+    <>
+      <div className='mt-24'>
+        <img src={Not} alt="Not Found" className='w-full'/>
+      </div>
+      
+    </>
+  )
+}
